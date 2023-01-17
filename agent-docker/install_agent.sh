45344ffc732f7cbc1b00e34cc9542f28b07c293b
endsof <endsof@gmail.com>
2023-01-17 19:24:17 +0500

    build: Откат Ubuntu до 18.04

--- agent-docker/install_agent.sh
+++
@@ -1,4 +0,0 @@
-echo "Downloading and installing Azure Pipelines agent..."
-
-curl -LS https://vstsagentpackage.azureedge.net/agent/2.213.2/vsts-agent-linux-x64-2.213.2.tar.gz | tar -xz & wait $!
-./bin/installdependencies.sh
